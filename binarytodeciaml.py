3f7d98c4b96c827e20012895183cfde64f9c560a
kanish12318 <Siyajindal376@gmail.com>
2026-01-31 21:28:03 +0530

    Add files via upload

---
+++ binarytodeciaml.py
@@ -0,0 +1,14 @@
+num1=10
+num2=4
+print("num1&num2=", num1 & num2)
+print("num1|num2=", num1 | num2)
+print("num1^num2=", num1 ^ num2)
+print("~num1=", ~num1)
+num1=10
+num2=4
+print("num1<<1=", num1 << 1)
+print("num2>>1=", num2 >> 1)
+num1=10
+num2=4
+print("num1>>1=", num1 >> 1)
+print("num2<<1=", num2 << 1)
